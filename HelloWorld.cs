7e3fe9edc576fc78d8bdea01156f14fa67f00cfb
Buti Mosia <mosia.mp@gmail.com>
2018-02-07 10:34:33 +0200

    Removed Xamarin.UITest packages

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -10,7 +10,7 @@ using Microsoft.AppCenter.Crashes;
 
 namespace Mono.Samples.HelloWorld
 {
-	[Activity (Label = "Hello Android World", MainLauncher = true)]
+	[Activity(Label = "Hello Android World", MainLauncher = true)]
 	[IntentFilter(new[] { Intent.ActionView },
 		Categories = new[] {
 			Intent.CategoryDefault,
@@ -19,15 +19,15 @@ namespace Mono.Samples.HelloWorld
 		DataScheme = "http",
 		DataHost = "example.com"
 	)]
-	public class HelloAndroid : Activity
+	public class HelloAndroid: Activity
 	{
 		protected override void OnCreate (Bundle savedInstanceState)
 		{
             AppCenter.Start("921e668d-d00c-4953-a8ab-a88fa5bae01f", typeof(Analytics), typeof(Crashes));
 
-			base.OnCreate (savedInstanceState);
+			base.OnCreate(savedInstanceState);
 
-			SetContentView (Resource.Layout.main);
+			SetContentView(Resource.Layout.main);
 		}
 	}
 }
